8326ea502c3b2b4f8dd67223fa5672fe3b0cf94c
Nick Sollon <genericjoke@gmail.com>
2023-01-02 03:43:41 -0500

    commit tree 68f6667f2743e6327042c7d91bf5e2f8307a5597

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const del = document.getElementById("main");
+del.remove();
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.innerHTML="Nick is the champion";
